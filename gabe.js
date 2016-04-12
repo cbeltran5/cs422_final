72b934766ac9536db748474649d0a5b6f8b50a92
Gabe <mendozagabe1618@icloud.com>
2016-04-12 11:45:40 -0500

    Adding quick support for language extension

--- gabe.js
+++ gabe.js
@@ -1,1 +1,13 @@
- 
+
+
+var SYSTEM_LANGUAGE = ["ENGLISH", "SPANISH"];
+var CAMERA = ["Camera", "Cámara"];
+var TRAFFIC = ["Traffic","Tráfico"];
+var TRANSPORT = ["Transport", "Transporte"];
+var NEWS = ["News", "Noticias"];
+var MIRROR = ["Mirror", "Espejo"];
+var LIGHTS = ["Lights", "Luces"];
+var ALARM = ["Alarm", "Alarma"];
+var EMERGENCY = ["Emergency", "Emergencia"];
+var SETTIGNS = ["Settings", "Ajustes"];
+
