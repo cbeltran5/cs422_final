0f04d4a8cc12deca6849e75dc80b56c25d2d0106
Gabe <mendozagabe1618@icloud.com>
2016-04-12 12:22:30 -0500

    Adding language extension to project in gabe.js

--- gabe.js
+++ gabe.js
@@ -1,6 +1,7 @@
-
-
+// variables for text
+// English = 1 , Spanish = 2, etc...
 var SYSTEM_LANGUAGE = ["ENGLISH", "SPANISH"];
+var MESSAGES = ["Messages", "Mensajes"];
 var CAMERA = ["Camera", "Cámara"];
 var TRAFFIC = ["Traffic","Tráfico"];
 var TRANSPORT = ["Transport", "Transporte"];
@@ -9,5 +10,20 @@ var MIRROR = ["Mirror", "Espejo"];
 var LIGHTS = ["Lights", "Luces"];
 var ALARM = ["Alarm", "Alarma"];
 var EMERGENCY = ["Emergency", "Emergencia"];
-var SETTIGNS = ["Settings", "Ajustes"];
+var SETTINGS = ["Settings", "Ajustes"];
+var DEADBOLT = ["Dead Bolt", "Cerrojo"];
 
+
+// months of the year
+var JANUARY = ["January", "Enero"];
+var FEBRUARY = ["February", "Febrero"];
+var MARCH = ["March", "Marzo"];
+var APRIL = ["April", "Abril"];
+var MAY = ["May","Mayo"];
+var JUNE = ["June", "Junio"];
+var JULY = ["July", "Julio"];
+var AUGUST = ["August", "Agosto"];
+var SEPTEMBER = ["September", "Septiembre"];
+var OCTOBER = ["October", "Octubre"];
+var NOVEMBER = ["November", "Noviembre"];
+var DECEMBER = ["December", "Diciembre"];
