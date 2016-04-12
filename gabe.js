8eeb0bb5edf2655dd0b271d58e65e7986f1de6ca
Gabe <mendozagabe1618@icloud.com>
2016-04-12 17:42:30 -0500

    Created a User class to easily instantiate new Users in the future.

--- gabe.js
+++ gabe.js
@@ -1,6 +1,10 @@
 // variables for text
 // English = 1 , Spanish = 2, etc...
+var ENGLISH = 0;
+var SPANISH = 1;
+
 var SYSTEM_LANGUAGE = ["ENGLISH", "SPANISH"];
+
 var MESSAGES = ["Messages", "Mensajes"];
 var CAMERA = ["Camera", "Cámara"];
 var TRAFFIC = ["Traffic","Tráfico"];
@@ -28,8 +32,38 @@ var OCTOBER = ["October", "Octubre"];
 var NOVEMBER = ["November", "Noviembre"];
 var DECEMBER = ["December", "Diciembre"];
 
-//
-//function UpdateText() 
-//{
-//    
-//}
+
+function UpdateText() 
+{
+  Messagestext.text = MESSAGES[syslang];
+  Cameratext.text = CAMERA[syslang];
+  Traffictext.text = TRAFFIC[syslang];
+  Ctatext.text = TRANSPORT[syslang];
+  Newstext.text = NEWS[syslang];
+  Mirrortext.text = MIRROR[syslang];
+  Lightstext.text = LIGHTS[syslang];
+  Alarmtext.text = ALARM[syslang];
+  Emergtext.text = EMERGENCY[syslang];
+  Settingstext.text = SETTINGS[syslang];
+  Deadbolttext.text = DEADBOLT[syslang];
+}
+
+
+// created a format
+function User (name, language) {
+  //console.log("User Instance created");
+  this.name = name;
+  this.language = language;
+  
+  this.date_format = 1;
+  this.time_format = 1;
+  this.units_format = 'Imperial';
+  
+  this.in_theme = 'wood';
+  this.out_theme = 'wood';
+  
+  this.in_ornament = 'default';
+  this.out_ornament = 'default';
+  console.log("Hello from " + this.name);
+}
+
